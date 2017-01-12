cd036ab844be5f23c021bc77fa103412311d8d02
Kirill Maksimov <kirill.maksimov@ironsrc.com>
2017-01-12 22:10:16 +0200

    Some people talk way too much

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ where rc_namespace = 0
 group by 1) b`).then(results => Math.round(results[0][0].num));
 };
 
-const mostProductiveMembers = () => {
+const getProductiveMembersForNamespace = (namespace) => {
     return knex.raw(`
 select rc_user, rc_user_text as name, sum(rc_new_len) as all_new, sum(rc_old_len) as all_old,
 sum((rc_new_len - rc_old_len)) as diff from recentchanges
-where rc_namespace = 0
+where rc_namespace = ${namespace}
 group by 1, 2
 having diff > 50000
 order by diff desc
@@ -25,22 +25,32 @@ order by diff desc
 const getStats = () => {
     return Promise.all([
         getAvgChangeNumber(),
-        mostProductiveMembers()
+        getProductiveMembersForNamespace(0),
+        getProductiveMembersForNamespace(90)
     ]);
+};
+
+const formatUsersAsList = (users) => {
+    const items = users.map(x => {
+        return `<li>Участник ${x.name} (${Math.round(x.diff / 1000)}kb)</li>`
+    }).join('\n');
+
+    return `<ol>${items}</ol>`;
 };
 
 app.get('/', (req, res) => {
     getStats().then((results) => {
         const avgChangeNumber = results[0],
-            productiveMembers = results[1].map(x => {
-                return `<li>Участник ${x.name} (${Math.round(x.diff / 1000)}kb)</li>`
-            }).join('\n'),
+            productiveMembers = formatUsersAsList(results[1]),
+            talkativeMembers = formatUsersAsList(results[2]),
             data = `
 <h1>Статистика за последний месяц</h1>
 Среднее количество правок в день: ${avgChangeNumber}</br></br>
-Пользователи со вкладом более 50 килобайт:<br/>
-<ol>${productiveMembers}</ol>`;
-
+Пользователи со вкладом более 50 килобайт (основное пространство):<br/>
+${productiveMembers}
+Пользователи со вкладом более 50 килобайт (обсуждения):<br/>
+${talkativeMembers}
+`;
         res.send(data);
     }).catch(() => {
         res.send(500, 'Internal server error');
